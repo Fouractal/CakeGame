98e55d16abfeb831398a463672d66d7f104ff26a
YunSeong <77562357+oeccsy@users.noreply.github.com>
2022-12-27 17:31:13 +0900

    feat : resource manage

---
+++ CakeGame/Assets/Scripts/Manager/ResourceManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceManager
+{
+    public ResourceManager() {}
+
+    public static T LoadAsset<T>(string resourcePath) where T : Object
+    {
+        // Resources 경로에서 탐색
+        T target = Resources.Load<T>($"{resourcePath}");
+
+        return target;
+    }
+}
