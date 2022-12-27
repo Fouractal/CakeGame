c5897654ed2c5212f42796279389275b435c532b
YunSeong <77562357+oeccsy@users.noreply.github.com>
2022-12-27 17:50:22 +0900

    add AreaInfo.cs

---
+++ CakeGame/Assets/Scripts/Data/AreaInfo.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaInfo
+{
+    public Define.CubeType cubeType = Define.CubeType.Choco;
+    public Cube cube;
+    public bool isDestroyed = false;
+}
