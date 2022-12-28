0a6942e68469dc989b08398c8e99b0c35adf01ee
YunSeong <77562357+oeccsy@users.noreply.github.com>
2022-12-28 15:06:16 +0900

    add AreaInfo row, column

--- CakeGame/Assets/Scripts/Data/AreaInfo.cs
+++ CakeGame/Assets/Scripts/Data/AreaInfo.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class AreaInfo
 {
+    public int rowIndex = 0;
+    public int columnIndex = 0;
+    
     public Define.CubeType cubeType;
     public Cube cube;
+    
     public bool isAimed = false;
+    public bool isPlayerJoin = false;
 }
