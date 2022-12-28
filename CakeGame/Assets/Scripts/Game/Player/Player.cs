77c7d033429bb9b839d7264ede30910a564e1832
YunSeong <77562357+oeccsy@users.noreply.github.com>
2022-12-28 17:53:01 +0900

    feat : BackgroundImage (sample image)

--- CakeGame/Assets/Scripts/Game/Player/Player.cs
+++ CakeGame/Assets/Scripts/Game/Player/Player.cs
@@ -6,7 +6,7 @@ public class Player
 {
     public static void InitPlayer()
     {
-        string resourcePath = $"Prefab/Player/Player.prefab";
+        string resourcePath = $"Prefab/Player/Player";
         GameObject playerPrefab = ResourceManager.LoadAsset<GameObject>(resourcePath);
         Object.Instantiate(playerPrefab, Vector3.up * 3, Quaternion.identity, GameObject.Find("Object").transform);
     }
