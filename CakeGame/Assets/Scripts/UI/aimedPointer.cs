d4385e403cd48551b50a7097d0ab9e278c417dc6
kkkkiiiiii <98201682+kkkkiiiiii@users.noreply.github.com>
2022-12-28 17:49:16 +0900

    feat : Aim Object

--- CakeGame/Assets/Scripts/UI/aimedPointer.cs
+++ CakeGame/Assets/Scripts/UI/aimedPointer.cs
@@ -6,6 +6,9 @@ using UnityEngine;
 
 public class aimedPointer : MonoBehaviour
 {
+    private Camera Cam;
+    private float rotation=90;
+    private float borderSize = 100f;
     private Vector3 targetPosition;
     private RectTransform pointerRectTransform;
     public Transform playerTransform;
@@ -16,33 +19,53 @@ public class aimedPointer : MonoBehaviour
     {
         get
         {
-            if (playerTransform.position.z < targetTransform.position.z)
+            if (playerTransform.position.z > targetTransform.position.z)
             {
-                _reverse = 1;
+                _reverse = -1;
             }
             else
-                _reverse = -1;
+                _reverse = 1;
             return _reverse;
         }
 
     }
     private void Awake()
     {
-        //targetPosition = new Vector3(200, 45);
         pointerRectTransform = transform.Find("Pointer").GetComponent<RectTransform>();
     }
 
     private void Update()
     {
+        // target 방향 표시
         Vector3 toPosition = targetTransform.position - Camera.main.transform.position;
         Vector3 fromPosition = playerTransform.position - Camera.main.transform.position;
         Vector3 dir = toPosition - fromPosition;
-        Debug.Log($"Camera : {Camera.main.transform.position}, player : {playerTransform.position}, fromPosition :{fromPosition}");
-        //playerTransform.gameObject.transform.position.normalized;
 
         float angle = Vector3.Angle(dir , Vector3.right);
-        pointerRectTransform.localEulerAngles = new Vector3(0, 0, angle * reverse);
-        Debug.Log($"target : {toPosition}, player : {fromPosition}, angle : {angle}");
-       //Debug.Log();
+        pointerRectTransform.localEulerAngles = Vector3.forward*rotation+new Vector3(0, 0, angle * reverse);
+
+        // Screen에 target있는가
+        Vector3 targetPositionScreenPoint = Camera.main.WorldToScreenPoint(targetTransform.position);
+        bool isOffScreen = targetPositionScreenPoint.x <= 0 || targetPositionScreenPoint.x >= Screen.width ||
+                           targetPositionScreenPoint.y <= 0 || targetPositionScreenPoint.y >= Screen.height;
+
+        if (isOffScreen)
+        {
+            
+            Vector3 cappedTargetScreenPosition = targetPositionScreenPoint;
+            if (cappedTargetScreenPosition.x <= borderSize) cappedTargetScreenPosition.x = borderSize; 
+            if (cappedTargetScreenPosition.x >= Screen.width - borderSize) cappedTargetScreenPosition.x = Screen.width - borderSize;
+            if (cappedTargetScreenPosition.y <= borderSize) cappedTargetScreenPosition.y = borderSize;
+            if (cappedTargetScreenPosition.y >= Screen.height - borderSize) cappedTargetScreenPosition.y = Screen.height - borderSize;
+            Vector3 pointWorldPosition = Camera.main.ScreenToWorldPoint(cappedTargetScreenPosition);
+            
+            pointerRectTransform.position = new Vector3(cappedTargetScreenPosition.x,
+                cappedTargetScreenPosition.y, 0f);
+        }
+        else
+        {
+            pointerRectTransform.position = new Vector3(targetPositionScreenPoint.x, targetPositionScreenPoint.y, 0f);
+        }
     }
+
 }
