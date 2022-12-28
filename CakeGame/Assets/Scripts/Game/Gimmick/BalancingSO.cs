390596f09c583a07c247d8d0216cbb568c49127a
YunSeong <77562357+oeccsy@users.noreply.github.com>
2022-12-28 16:52:08 +0900

    add balancing scriptable object

---
+++ CakeGame/Assets/Scripts/Game/Gimmick/BalancingSO.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Game.Gimmick
+{
+    [CreateAssetMenu(fileName = "BalancingSO", menuName = "ScripatableObject/Balnancing", order = 0)]
+    public class BalancingSO : ScriptableObject
+    {
+        public float forkSpawnDelayMin;
+        public float forkSpawnDelayMax;
+        public float attackDelayMin;
+        public float attackDelayMax;
+
+        public int forkCountMax;
+    }
+}
