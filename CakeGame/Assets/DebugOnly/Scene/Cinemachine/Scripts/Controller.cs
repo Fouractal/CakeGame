1692fc3901b3ce55ed7fff5953dc3e133a223c19
kkkkiiiiii <98201682+kkkkiiiiii@users.noreply.github.com>
2023-01-04 23:29:18 +0900

    feat : DebugOnly, Cinemachine-StateDrivenCamera

--- CakeGame/Assets/DebugOnly/Scene/Cinemachine/Scripts/Controller.cs
+++ CakeGame/Assets/DebugOnly/Scene/Cinemachine/Scripts/Controller.cs
@@ -1,24 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
-
+using DG.Tweening;
 public class Controller : MonoBehaviour
 {
     private CharacterController _characterController;
+    private Animator _playerAnimator;
     private Vector3 _playerVelocity;
     [SerializeField] private bool _groundedPlayer;
-    private float _playerSpeed = 2.0f;
+    [SerializeField] private float _playerSpeed = 2.0f;
     private float _jumpHeight = 1.0f;
     private float _gravityValue = -9.81f;
+    [SerializeField] private float _strawberryDist= 3f;
+    public Transform StrawberryTransform;
+    public Transform RightHand;
+    public bool Ray;
+    private Ray _ray;
+    public LayerMask LayerMask;
+    public RaycastHit hit;
 
+    
     private void Start()
     {
         _characterController = gameObject.GetComponent<CharacterController>();
+        _playerAnimator = GetComponent<Animator>();
     }
 
     void Update()
     {
         _groundedPlayer = _characterController.isGrounded;
+        _ray = new Ray(transform.position + Vector3.up, transform.forward);
+        Ray = Physics.Raycast(_ray, out hit, 5f, LayerMask);
+        
         if (_groundedPlayer && _playerVelocity.y < 0)
         {
             _playerVelocity.y = 0f;
@@ -40,5 +54,46 @@ public class Controller : MonoBehaviour
 
         _playerVelocity.y += _gravityValue * Time.deltaTime;
         _characterController.Move(_playerVelocity * Time.deltaTime);
+        if (Input.GetAxis("Horizontal") != 0 || (Input.GetAxis("Vertical"))!= 0 )
+        {
+            _playerAnimator.SetBool("walk",true);
+        }
+        else
+        {
+            _playerAnimator.SetBool("walk",false);
+        }
+
+        if (Input.GetKey("space") && (transform.position - StrawberryTransform.position).sqrMagnitude < _strawberryDist
+            && Ray) // 조건 추가 : 일정 거리안에 있어야하고 player가 딸기를 보고 있어야 한다. 
+        {
+            GetStrawberry();
+        }
+
+        DrawRayLine();
     }
+    [ContextMenu("getStrawberry")]
+    private void GetStrawberry()
+    {
+        _playerAnimator.SetTrigger("getStrawberry");
+        StrawberryTransform.DOScale(Vector3.one * 0.1f, 4.7f);
+        StrawberryTransform.SetParent(RightHand);
+        StrawberryTransform.transform.localPosition =
+            Vector3.Lerp(StrawberryTransform.localPosition, Vector3.zero, 2.7f);
+        //StrawberryTransform.DOMove(RightHand.position, 4.7f); // 정확히 0으로 안감 애니메이션에 치명적
+        //yield return new WaitForSeconds(4.7f); // 줍는 애니메이션 길이 
+
+    }
+
+    public void DrawRayLine()
+    {
+        if (hit.collider != null)
+        {
+            Debug.DrawRay(transform.position +Vector3.up,transform.forward * hit.distance,Color.red);
+        }
+        else
+        {
+            Debug.DrawRay(transform.position +Vector3.up,transform.forward * 5, Color.red);
+        }
+    }
+    
 }
