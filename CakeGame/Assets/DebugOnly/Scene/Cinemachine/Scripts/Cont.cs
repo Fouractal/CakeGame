76090bd2d9147ef6e34215ba343f8a7699b260d6
kkkkiiiiii <98201682+kkkkiiiiii@users.noreply.github.com>
2023-01-05 20:10:47 +0900

    feat : DebugOnly
    Jump 1,2,3
    juumpCouunt에 따라
    점프 높이, 애니메이션 다르게

--- CakeGame/Assets/DebugOnly/Scene/Cinemachine/Scripts/Cont.cs
+++ CakeGame/Assets/DebugOnly/Scene/Cinemachine/Scripts/Cont.cs
@@ -6,23 +6,48 @@ using UnityEngine.InputSystem;
 
 public class Cont : MonoBehaviour
 {
-    // declare reference variables
+
+    #region declare reference variables
     private PlayerInput _playerInput;
     private CharacterController _characterController;
     private Animator _animator;
-    
-    // vaiables to store optimized setter/getter parameter IDs
+    #endregion
+
+    #region vaiables to store optimized setter/getter parameter IDs
     private int _isWalkingHash;
     private int _isRunningHash;
-    
-    // variables to store player input values
+    private int _isJumpingHash;
+    private int _jumpCountHash;
+    #endregion
+
+    #region variables to store player input values
     private Vector2 _currentMovementInput;
     private Vector3 _currentMovement;
     private Vector3 _currentRunMovement;
     private bool _isMovementPressed;
     private bool _isRunPressed;
+    #endregion
+
+    #region constants
     private float _rotationFactorPerFrame = 10f;
     private float _runMultiplier = 3f;
+    float _groundedGravity = -0.05f;
+    private float _gravity = -9.8f;
+    #endregion
+
+    #region Jumping variables
+    private bool _isJumpPressed = false;
+    private bool _isJumping = false;
+    private float _initialJumpVelocity;
+    private float _maxJumpHeight = 4f;
+    private float _maxJumpTime = 0.75f;
+    private bool _isJumpAnimating = false;
+    private int _jumpCount = 0;
+    private Dictionary<int, float> _initialJumpVelocities = new Dictionary<int, float>();
+    private Dictionary<int, float> _jumpGravities = new Dictionary<int, float>();
+    private Coroutine _currentJumpResetRoutine = null;
+    #endregion
+    
     private void Awake()
     {
         _playerInput = new PlayerInput();
@@ -31,15 +56,21 @@ public class Cont : MonoBehaviour
 
         _isRunningHash = Animator.StringToHash("run");
         _isWalkingHash = Animator.StringToHash("walk");
+        _isJumpingHash = Animator.StringToHash("isJumping");
+        _jumpCountHash = Animator.StringToHash("jumpCount");
         
         _playerInput.CharacterControls.Move.started += OnMovementInput;
         _playerInput.CharacterControls.Move.canceled += OnMovementInput;
         _playerInput.CharacterControls.Move.performed += OnMovementInput;
         _playerInput.CharacterControls.Run.started += OnRun;
         _playerInput.CharacterControls.Run.canceled += OnRun;
+        _playerInput.CharacterControls.Jump.started += OnJump;
+        _playerInput.CharacterControls.Jump.canceled += OnJump;
         // "started" is specifically called when the input system first receives the input. But that's it
         // If we wanna track when the key is let go, we need to use the "Canceled" callback.
         // "Performed" will continue to update the changes in the player input
+
+        SetUpJumpVariables();
     }
 
     void OnMovementInput(InputAction.CallbackContext context)
@@ -57,10 +88,35 @@ public class Cont : MonoBehaviour
         _isRunPressed = context.ReadValueAsButton();
     }
 
+    void OnJump(InputAction.CallbackContext context)
+    {
+        _isJumpPressed = context.ReadValueAsButton();
+    }
+
+    void SetUpJumpVariables()
+    {
+        float timeToApex = _maxJumpTime / 2;
+        _gravity = (-2 * _maxJumpHeight) / Mathf.Pow(timeToApex, 2);
+        _initialJumpVelocity = (2 * _maxJumpHeight) / timeToApex;
+        
+        float secondJumpGravity = (-2* (_maxJumpHeight + 2)) / Mathf.Pow((timeToApex*1.25f),2);
+        float secondJumpInitialVelocity = (2 * (_maxJumpHeight * 2)) / (timeToApex * 1.5f);
+        float thirdJumpGravity = (-2* (_maxJumpHeight + 4)) / Mathf.Pow((timeToApex*1.5f),2);
+        float thirdJumpInitialVelocity = (2 * (_maxJumpHeight * 4)) / (timeToApex * 2f);
+        
+        _initialJumpVelocities.Add(1,_initialJumpVelocity);
+        _initialJumpVelocities.Add(2,secondJumpInitialVelocity);
+        _initialJumpVelocities.Add(3,thirdJumpInitialVelocity);
+        
+        _jumpGravities.Add(0,_gravity);
+        _jumpGravities.Add(1,_gravity);
+        _jumpGravities.Add(2,secondJumpGravity);
+        _jumpGravities.Add(3,thirdJumpGravity);
+    }
+    
     // Update is called once per frame
     void Update()
     {
-        HandleGravity();
         HandleRotation();
         HandleAnimation();
         if (_isRunPressed)
@@ -71,6 +127,8 @@ public class Cont : MonoBehaviour
         {
             _characterController.Move(_currentMovement * Time.deltaTime);    
         }
+        HandleGravity();
+        HandleJump();
     }
 
     private void HandleAnimation()
@@ -115,18 +173,70 @@ public class Cont : MonoBehaviour
     }
     private void HandleGravity()
     {
+        bool isFalling = _currentMovement.y <= 0.0f || !_isJumpPressed;
+        float fallMultiplier = 2.0f;
+        
         if (_characterController.isGrounded)
         {
-            float groundedGravity = -0.05f;
-            _currentMovement.y = groundedGravity;
-            _currentRunMovement.y = groundedGravity;
+            if (_isJumpAnimating)
+            {
+                _animator.SetBool(_isJumpingHash,false);
+                _isJumpAnimating = false;
+                _currentJumpResetRoutine = StartCoroutine(JumpResetRoutine());
+                if (_jumpCount == 3)
+                {
+                    _jumpCount = 0;
+                    _animator.SetInteger(_jumpCountHash,_jumpCount);
+                }
+            }
+            _currentMovement.y = _groundedGravity;
+            _currentRunMovement.y = _groundedGravity;
+        }else if (isFalling)
+        {
+            float previousVelocityY = _currentMovement.y;
+            float newVelocityY = _currentMovement.y + (_jumpGravities[_jumpCount] * fallMultiplier * Time.deltaTime);
+            float nextVelocityY = Mathf.Max((previousVelocityY + newVelocityY) * .5f,-20.0f) ;
+            _currentMovement.y = nextVelocityY;
+            _currentRunMovement.y = nextVelocityY;
         }
         else
-        {
-            float gravity = -9.8f;
-            _currentMovement.y = gravity;
-            _currentRunMovement.y = gravity;
+        {   // 항상 같은 높이뛰기 위함
+            float previousVelocityY = _currentMovement.y;
+            float newVelocityY = _currentMovement.y + (_jumpGravities[_jumpCount] * Time.deltaTime);
+            float nextVelocityY = (previousVelocityY + newVelocityY) * .5f;
+            _currentMovement.y = nextVelocityY;
+            _currentRunMovement.y = nextVelocityY;
         }
+    }
+
+    private void HandleJump()
+    {
+        if (!_isJumping &&  _characterController.isGrounded && _isJumpPressed)
+        {
+            if (_jumpCount<3 && _currentJumpResetRoutine != null)
+            {
+                StopCoroutine(_currentJumpResetRoutine);   
+            }
+            _animator.SetBool(_isJumpingHash,true);
+            _isJumpAnimating = true;
+            _isJumping = true;
+            _jumpCount += 1;
+            _animator.SetInteger(_jumpCountHash,_jumpCount);
+            float previousVelocityY = _currentMovement.y;
+            float newVelocityY = (_currentMovement.y + _initialJumpVelocity);
+            float nextVelocityY = (previousVelocityY + newVelocityY) * .5f;
+            _currentMovement.y = _initialJumpVelocities[_jumpCount] * .5f;
+            _currentRunMovement.y = _initialJumpVelocities[_jumpCount] * .5f;
+        }else if (_isJumping && _characterController.isGrounded && !_isJumpPressed)
+        {
+            _isJumping = false;
+        }
+    }
+
+    IEnumerator JumpResetRoutine()
+    {
+        yield return new WaitForSeconds(.5f);
+        _jumpCount = 0;
     }
     private void OnEnable()
     {
--- CakeGame/Assets/DebugOnly/Scene/Cinemachine/Scripts/playerInput.cs
+++ CakeGame/Assets/DebugOnly/Scene/Cinemachine/Scripts/playerInput.cs
@@ -44,6 +44,15 @@ public partial class @PlayerInput : IInputActionCollection2, IDisposable
                     ""processors"": """",
                     ""interactions"": """",
                     ""initialStateCheck"": false
+                },
+                {
+                    ""name"": ""Jump"",
+                    ""type"": ""Button"",
+                    ""id"": ""8bf57a57-0c6b-4377-a724-c6e07177c0cd"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""initialStateCheck"": false
                 }
             ],
             ""bindings"": [
@@ -112,6 +121,17 @@ public partial class @PlayerInput : IInputActionCollection2, IDisposable
                     ""action"": ""Run"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""d5b4d4ec-c1e4-453b-b56d-7d61e7a2e79f"",
+                    ""path"": ""<Keyboard>/space"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
                 }
             ]
         }
@@ -122,6 +142,7 @@ public partial class @PlayerInput : IInputActionCollection2, IDisposable
         m_CharacterControls = asset.FindActionMap("CharacterControls", throwIfNotFound: true);
         m_CharacterControls_Move = m_CharacterControls.FindAction("Move", throwIfNotFound: true);
         m_CharacterControls_Run = m_CharacterControls.FindAction("Run", throwIfNotFound: true);
+        m_CharacterControls_Jump = m_CharacterControls.FindAction("Jump", throwIfNotFound: true);
     }
 
     public void Dispose()
@@ -183,12 +204,14 @@ public partial class @PlayerInput : IInputActionCollection2, IDisposable
     private ICharacterControlsActions m_CharacterControlsActionsCallbackInterface;
     private readonly InputAction m_CharacterControls_Move;
     private readonly InputAction m_CharacterControls_Run;
+    private readonly InputAction m_CharacterControls_Jump;
     public struct CharacterControlsActions
     {
         private @PlayerInput m_Wrapper;
         public CharacterControlsActions(@PlayerInput wrapper) { m_Wrapper = wrapper; }
         public InputAction @Move => m_Wrapper.m_CharacterControls_Move;
         public InputAction @Run => m_Wrapper.m_CharacterControls_Run;
+        public InputAction @Jump => m_Wrapper.m_CharacterControls_Jump;
         public InputActionMap Get() { return m_Wrapper.m_CharacterControls; }
         public void Enable() { Get().Enable(); }
         public void Disable() { Get().Disable(); }
@@ -204,6 +227,9 @@ public partial class @PlayerInput : IInputActionCollection2, IDisposable
                 @Run.started -= m_Wrapper.m_CharacterControlsActionsCallbackInterface.OnRun;
                 @Run.performed -= m_Wrapper.m_CharacterControlsActionsCallbackInterface.OnRun;
                 @Run.canceled -= m_Wrapper.m_CharacterControlsActionsCallbackInterface.OnRun;
+                @Jump.started -= m_Wrapper.m_CharacterControlsActionsCallbackInterface.OnJump;
+                @Jump.performed -= m_Wrapper.m_CharacterControlsActionsCallbackInterface.OnJump;
+                @Jump.canceled -= m_Wrapper.m_CharacterControlsActionsCallbackInterface.OnJump;
             }
             m_Wrapper.m_CharacterControlsActionsCallbackInterface = instance;
             if (instance != null)
@@ -214,6 +240,9 @@ public partial class @PlayerInput : IInputActionCollection2, IDisposable
                 @Run.started += instance.OnRun;
                 @Run.performed += instance.OnRun;
                 @Run.canceled += instance.OnRun;
+                @Jump.started += instance.OnJump;
+                @Jump.performed += instance.OnJump;
+                @Jump.canceled += instance.OnJump;
             }
         }
     }
@@ -222,5 +251,6 @@ public partial class @PlayerInput : IInputActionCollection2, IDisposable
     {
         void OnMove(InputAction.CallbackContext context);
         void OnRun(InputAction.CallbackContext context);
+        void OnJump(InputAction.CallbackContext context);
     }
 }
